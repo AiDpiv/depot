dd30a93a71d67d812ff4466f1b0dfcaa72048805
AiDpiv <aidpiv@gmail.com>
2018-03-01 21:49:20 +0200

    ajax finaly done

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -61,7 +61,7 @@ rescue_from ActiveRecord::RecordNotFound, with: :invalid_cart
     session[:cart_id] = nil
 
     respond_to do |format|
-      format.html { redirect_to store_index_url, notice: 'Your cart is empty now' }
+      format.html { redirect_to store_index_url}
       format.json { head :no_content }
     end
   end
